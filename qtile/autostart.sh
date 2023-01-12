a172ebd4af483fc2c3c0ae27be40d35588f076e7
Khatermm <lawicki02@gmail.com>
2023-01-12 13:52:21 +0100

    qutile and kitty

---
+++ qtile/autostart.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+kitty &
+#firefox
+picom -f &
+#volumeicon &
+sudo NetworkManager &
+nm-applet &
+#~/.fehbg &
+nitrogen --restore &
+#xrandr --output HDMI-1-1 --mode 1920x1080 --output eDP-1 --mode 1920x1080 --left-of HDMI-1-1
